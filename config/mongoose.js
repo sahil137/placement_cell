f8684d73edc3c42299b5cb7c275b84525e3cd947
Sahil Saini <sahilsaini137@gmail.com>
2022-01-23 20:26:55 +0530

    add .env and procfile for heroku deployement

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect('mongodb://localhost/placement_cell');
+dotenv.config({ path: 'config/.env' });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const db = require('./config/mongoose');
-const app = express();
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-startegy');
 
-const PORT = 8000;
+dotenv.config({ path: 'config/.env' });
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 
 // set ejs as view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.use(
   session({
-    secret: 'randomsecretkey',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 100 },
@@ -24,6 +23,7 @@ app.use(
 
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+app.use(express.urlencoded({ extended: true }));
 
 // for authentication
 app.use(passport.initialize());
@@ -33,10 +33,13 @@ app.use(passport.setAuthenticatedUser);
 // express router
 app.use('/', require('./routes'));
 
-app.listen(PORT, function (error) {
+const port = 8000;
+
+// listen on port
+app.listen(port, function (error) {
   if (error) {
     console.log(`Error in connecting to server: ${error}`);
     return;
   }
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${port}`);
 });
